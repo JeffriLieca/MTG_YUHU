fda3a12d0ab967d4c89da0b3f29ab3ece662f3ed
JeffriLieca <95264893+JeffriLieca@users.noreply.github.com>
2023-11-17 22:34:52 +0700

    Add files via upload

---
+++ MTG_Yuhu/SwiftUIView.swift
@@ -0,0 +1,20 @@
+////
+////  SwiftUIView.swift
+////  MTG_Yuhu
+////
+////  Created by MacBook Pro on 17/11/23.
+////
+//
+//import SwiftUI
+//
+//extension UINavigationController {
+//    open override func viewDidAppear(_ animated: Bool) {
+//        super.viewDidAppear(animated)
+//        let appearance = UINavigationBarAppearance()
+//        appearance.configureWithOpaqueBackground()
+//        appearance.backgroundColor = UIColor.systemBlue // Atur warna latar belakang
+//        navigationBar.standardAppearance = appearance
+//        navigationBar.compactAppearance = appearance
+//        navigationBar.scrollEdgeAppearance = appearance
+//    }
+//}
